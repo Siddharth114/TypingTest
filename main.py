0d20fb695e9516badce27f91947e8ca21a4496eb
Siddharth <2004sid@gmail.com>
2021-11-11 16:01:14 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import curses
+from curses import wrapper
+
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr('Welcome to my typing test! \nPress any key to continue.')
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+def main(stdscr):
+    curses.init_pair(1,curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2,curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3,curses.COLOR_WHITE, curses.COLOR_BLACK)
+    
+    start_screen(stdscr)
+
+
+wrapper(main)
